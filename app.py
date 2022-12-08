c1f4091897b865aec372f04491676aba3a877174
Melvin <melvinjoash@gmail.com>
2022-12-08 15:02:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -100,5 +100,8 @@ def listing():
             print(f'{key}: {value}')
     return render_template('listing.html')
 
+    else:
+        return render_template('listing.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
