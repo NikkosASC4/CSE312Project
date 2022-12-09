f5970d8e2dec8f7d7f622b2206ca94379cf79a4f
Melvin <melvinjoash@gmail.com>
2022-12-08 22:19:14 -0500

    websockets

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,6 +1,17 @@
 const socket = new WebSocket('ws://' + location.host + '/echo');
 
+const log = (text, color) => {
+        document.getElementById('log').innerHTML += `<span style="color: ${color}">${text}</span><br>`;
+			};
 
-function welcome() {
-	document.getElementById("paragraph").innerHTML += "<br/>This text was added by JavaScript 😀"
-}
+const socket = new WebSocket('ws://' + location.host + '/echo');
+      socket.addEventListener('message', ev => {
+        log('<<< ' + ev.data, 'blue');
+      });
+      document.getElementById('form').onsubmit = ev => {
+        ev.preventDefault();
+        const textField = document.getElementById('text');
+        log('>>> ' + textField.value, 'red');
+        socket.send(textField.value);
+        textField.value = '';
+      };
