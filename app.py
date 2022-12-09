049d5bc767c31b5aded751ab47a39494e25f8980
Muhammad Hamza Niaz <hamzaniaz9856@gmail.com>
2022-12-09 05:53:21 -0500

    Debugging my implementation, anyone else feel free to take a look. I am going to sleep

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import bcrypt
 import os
 import random
 import hashlib
-from flask_login import LoginManager, UserMixin, login_required, login_user
+
+# from flask_login import LoginManager, UserMixin, login_required, login_user
 
 # Declare Database
 mongo_client = pymongo.MongoClient("mongo")
@@ -16,8 +17,9 @@ listings = db["listings"]
 
 app = Flask(__name__)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
 
 # sock = Sock(app)
@@ -26,35 +28,6 @@ login_manager.init_app(app)
 @app.route('/', methods=["GET", "POST"])
 def index():
     return redirect(url_for('home_page'))
-
-
-# Create a class that represents a user
-class User(UserMixin):
-    def __init__(self, username, password_hash):
-        self.username = username
-        self.password_hash = password_hash
-
-
-@login_manager.user_loader
-def load_user(username):
-    # Retrieve the user's information from the database
-    user = db.userAccounts.find_one({'username': username})
-
-    if user:
-        # Create a user object
-        return User(user['username'], user['password'])
-    else:
-        # Return None if the user does not exist
-        return None
-
-
-@app.route('/profile')
-@login_required
-def profile():
-    # Get the logged-in user's username from the session
-    username = session['user_id']
-
-    return render_template('profile.html', username=username)
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -137,34 +110,59 @@ def cart():
         return render_template('cart.html', carter=carthistory)
 
 
-@app.route('/settings', methods=["GET", "POST"])
-@login_required
-def settings():
-    if request.method == 'POST':
-        # Get the new username and password from the request
-        new_username = request.form['username']
-        new_password = (request.form['password']).encode()
-        # Validate input with database
-        # Get the current user's information from the database
-        current_user = db.users.find_one({'username': request.form['username']})
-        # Check if the current user is the owner of the data
-        if current_user['id'] == request.form['user_id']:
-            # Get the user's information from the database
-            user = userAccounts.find_one({'username': request.form['username']})
-            user['username'] = new_username
-            generatedSalt = bcrypt.gensalt(10)
-            hashed_password = bcrypt.hashpw(new_password, generatedSalt)
-            userAccounts.update_one({'username': request.form['username']},
-                                    {'$set': {'username': new_username, 'password': hashed_password}})
-            return 'Successfully updated username and password!'
-        else:
-            return "You are not authorized to edit this user's data!"
+# # Create a class that represents a user
+# class User(UserMixin):
+#     def __init__(self, username, password_hash):
+#         self.username = username
+#         self.password_hash = password_hash
+#
+#
+# @login_manager.user_loader
+# def load_user(username):
+#     # Retrieve the user's information from the database
+#     user = db.userAccounts.find_one({'username': username})
+#
+#     if user:
+#         # Create a user object
+#         return User(user['username'], user['password'])
+#     else:
+#         # Return None if the user does not exist
+#         return None
 
 
 @app.route('/profile', methods=["GET", "POST"])
+# @login_required
 def profile():
-    if request.method == 'POST':
-        return render_template('profile.html')
+    # Get the logged-in user's username from the session
+    username = session['username']
+
+    return render_template('profile.html', username=username)
+
+
+@app.route('/settings', methods=["POST"])
+# @login_required
+def settings():
+    # if request.method == 'POST':
+    # Get the new username and password from the request
+    new_username = request.form['username']
+    new_password = (request.form['password']).encode()
+    # Validate input with database
+    # Get the current user's information from the database
+    current_user = db.users.find_one({'username': request.form['username']})
+
+    user = userAccounts.find_one({'username': request.form['username']})
+    user['username'] = new_username
+    generatedSalt = bcrypt.gensalt(10)
+    hashed_password = bcrypt.hashpw(new_password, generatedSalt)
+    userAccounts.update_one({'username': request.form['username']},
+                            {'$set': {'username': new_username, 'password': hashed_password}})
+    return 'Successfully updated username and password!'
+    # Check if the current user is the owner of the data
+    # if current_user['username'] == request.form['username']:
+    # Get the user's information from the database
+
+    # else:
+    #     return "You are not authorized to edit this user's data!"
 
 
 @app.route('/listing', methods=["GET", "POST"])
