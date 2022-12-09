0233b8b554e44282a079d14890e17891504f050c
Muhammad Hamza Niaz <hamzaniaz9856@gmail.com>
2022-12-09 13:59:17 -0500

    UI listing fix

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-
-#Declare Database
+# Declare Database
 
 # from flask_login import LoginManager, UserMixin, login_required, login_user
 
@@ -20,10 +19,8 @@ db = mongo_client["cse312"]
 userAccounts = db["accounts"]
 authTokens = db["tokens"]
 
-cartz= db["cartloz"]
+cartz = db["cartloz"]
 listings = db["listerlolz"]
-
-
 
 app = Flask(__name__)
 
@@ -88,42 +85,48 @@ def home_page():
 
 @app.route('/buy', methods=["GET", "POST"])
 def buy():
-    buystuff=""
-    lister=listings.find({})
-    itemlist=""
+    buystuff = ""
+    lister = listings.find({})
+    itemlist = ""
 
     for p in lister:
         print(str(p["image"]))
-        itemlist=itemlist+'<div class="item-listing"><form method="post" action="/cart" enctype="multipart/form-data"><img src="data:image/jpeg;base64,'+str(p["image"])+'" alt="Insert Alt Text" style="width:100%;height:300px"> <p class="name">'+str(p["Name"])+'</p>'+'<p class="price">'+str(p["Price"])+'</p>'+'<input id="item-name" name="item-name" type="hidden" value="'+str(p["Name"])+'"/>'+'<input name="item-price" id="item-price" type="hidden" value="'+str(p["Price"])+'"\>'+'<input id="item-desc" name="item-desc"type="hidden" value="'+str(p["Discription"])+'"\>'+'<input type="submit" value="Post"/></form></div>'
-    return render_template('buy.html',shop=itemlist)
+        itemlist = itemlist + '<div class="item-listing"><form method="post" action="/cart" enctype="multipart/form-data"><img src="data:image/jpeg;base64,' + str(
+            p["image"]) + '" alt="Insert Alt Text" style="width:100%;height:300px"> <p class="name">' + str(
+            p["Name"]) + '</p>' + '<p class="price">' + str(
+            p["Price"]) + '</p>' + '<input id="item-name" name="item-name" type="hidden" value="' + str(
+            p["Name"]) + '"/>' + '<input name="item-price" id="item-price" type="hidden" value="' + str(
+            p["Price"]) + '"\>' + '<input id="item-desc" name="item-desc"type="hidden" value="' + str(
+            p["Discription"]) + '"\>' + '<input type="submit" value="Post"/></form></div>'
+    return render_template('buy.html', shop=itemlist)
 
 
 @app.route('/cart', methods=["GET", "POST"])
 def cart():
     if request.method == 'POST':
-         Name = request.form['item-name']
-         Discription=request.form['item-desc']
-         price=request.form['item-price']
-         cartz.insert_one({"Item": Name, "Discription": Discription, "Price":price})
+        Name = request.form['item-name']
+        Discription = request.form['item-desc']
+        price = request.form['item-price']
+        cartz.insert_one({"Item": Name, "Discription": Discription, "Price": price})
 
-         print("HAHAHAHAH")
-         print(price)
-         return redirect(url_for('buy'))
+        print("HAHAHAHAH")
+        print(price)
+        return redirect(url_for('buy'))
 
     else:
         mongoz = cartz.find({})
         carthistory = ""
         grandtotal = 0
         for p in mongoz:
-            carthistory=carthistory+'<tr><td>'+str(p["Item"])+'</td>'
-            carthistory=carthistory+'<td>'+"$"+str(p["Price"])+'</td>'
-            carthistory=carthistory+'<td>'+str(p["Discription"])+'</td><td>'+"$"+str(p["Price"])+'</td></tr>'
-            grandtotal=grandtotal+int(p["Price"])
-        carthistory=carthistory+'<tr><td>'+"Grand Total is"+'</td>'
-        carthistory=carthistory+'<td></td><td></td>'
-        carthistory=carthistory+'<td>'+"$"+str(grandtotal)+'</td></tr>'
-        return render_template('cart.html',carter=carthistory)
-
+            carthistory = carthistory + '<tr><td>' + str(p["Item"]) + '</td>'
+            carthistory = carthistory + '<td>' + "$" + str(p["Price"]) + '</td>'
+            carthistory = carthistory + '<td>' + str(p["Discription"]) + '</td><td>' + "$" + str(
+                p["Price"]) + '</td></tr>'
+            grandtotal = grandtotal + int(p["Price"])
+        carthistory = carthistory + '<tr><td>' + "Grand Total is" + '</td>'
+        carthistory = carthistory + '<td></td><td></td>'
+        carthistory = carthistory + '<td>' + "$" + str(grandtotal) + '</td></tr>'
+        return render_template('cart.html', carter=carthistory)
 
 
 # # Create a class that represents a user
@@ -150,8 +153,7 @@ def cart():
 # @login_required
 def profile():
     # Get the logged-in user's username from the session
-    username = userAccounts.find_one({'username': request.form['username']})
-    return render_template('profile.html', user=username)
+    return render_template('profile.html')
 
 
 @app.route('/settings', methods=['POST', 'GET'])
@@ -185,13 +187,13 @@ def settings():
 @app.route('/listing', methods=["GET", "POST"])
 def listing():
     if request.method == 'POST':
-         Name = request.form['item-name']
-         Discription=request.form['item-desc']
-         price=request.form['item-price']
-         file = request.files['file'].read()
-         listings.insert_one({"Name": Name, "Price": price, "Discription":Discription, "image":file})
+        Name = request.form['item-name']
+        Discription = request.form['item-desc']
+        price = request.form['item-price']
+        file = request.files['file'].read()
+        listings.insert_one({"Name": Name, "Price": price, "Discription": Discription, "image": file})
 
-         return redirect(url_for('buy'))
+        return redirect(url_for('buy'))
 
     else:
         # cursor = listings.find({})
